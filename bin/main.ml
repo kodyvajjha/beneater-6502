660d4eb60b570e1774bede65379f2ce78f71a247
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2024-02-28 20:47:47 -0800

    wip

--- bin/main.ml
+++ bin/main.ml
@@ -6,35 +6,32 @@ let src = Logs.Src.create "eater.cpu" ~doc:"logs Eater CPU events"
 
 module Log = (val Logs.src_log src : Logs.LOG)
 
-(* open Stdint *)
-
 (*
-       Want to capture one loop of the wozmon repl.
-       Input is logged at location 0x200
-       We read the output which is logged to ACIA_DATA at 0x5000.
-    *)
+  Want to capture one loop of the wozmon repl.
+  Input is logged at location 0x200
+  We read the output which is logged to ACIA_DATA at 0x5000.
+*)
 
-(* let rec run_cpu cpu =
-   let open Beneater_6502.Cpu in
-   EaterCPU.next_instruction cpu *)
-(* module Eater = struct
-     [@@@warning "-32"]
+module Eater = struct
+  [@@@warning "-32"]
 
-     open Beneater_6502
+  open Beneater_6502
 
-     type state = {
-       cpu: Cpu.t;
-       via: Via.t;
-     }
+  type state = {
+    cpu: Cpu.t;
+    display: Display.t;
+  }
 
-     let run (s : state) = Cpu.run s.cpu
-   end *)
+  let run (s : state) = Cpu.run s.cpu
+end
 
 let repl () =
   let open Beneater_6502 in
   let input : Cpu.EaterMemoryMap.input = { rom_path = "bin/wozmon.bin" } in
-  let cpu = Cpu.create input in
-  Lwt.join [ Cpu.run cpu; Via.write cpu; Display.show cpu ]
+  let state : Eater.state =
+    { cpu = Cpu.create input; display = { text = '*' } }
+  in
+  Lwt.join [ Eater.run state; Via.write state.cpu; Display.show state.cpu ]
 
 let () =
   Logs.set_reporter (Logs_fmt.reporter ());
--- lib/display.ml
+++ lib/display.ml
@@ -4,8 +4,14 @@ type t = { mutable text: char }
 
 let pp fpf t = CCFormat.fprintf fpf "%c" t.text
 
-let show (cpu : Cpu.t) =
-  let* () = Lwt_unix.sleep 3. in
+let read cpu =
   let* old = Lwt.return @@ Cpu.read_mem cpu 0x5004 in
-  let* _v = Lwt.return @@ { text = Char.chr (Stdint.Uint8.to_int old) } in
-  Lwt_io.printl (CCFormat.sprintf "HEREE!!!!!! %d" (Stdint.Uint8.to_int old))
+  Lwt.return @@ { text = Char.chr (Stdint.Uint8.to_int old) }
+
+let show (cpu : Cpu.t) =
+  let open Stdint in
+  let* () = Lwt_unix.sleep 3. in
+  let* { text } = read cpu in
+  Lwt_io.printl
+    (CCFormat.sprintf "HEREE!!!!!! %a" C6502.Utils.pp_u8
+       (Uint8.of_int (Char.code text)))
